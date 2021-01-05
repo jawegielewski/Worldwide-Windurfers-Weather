09b1e326cb7f6e714d63dc86622e5600a6f5fa7c
Jakub Węgielewski <jawegielewski@gmail.com>
2021-01-05 22:48:39 +0100

    Changed target annotation value

--- src/main/java/pl/jawegiel/worldwidewindsurfersweather/validators/DateWithinForecast.java
+++ src/main/java/pl/jawegiel/worldwidewindsurfersweather/validators/DateWithinForecast.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = DateWithinForecastValidator.class)
 public @interface DateWithinForecast {
